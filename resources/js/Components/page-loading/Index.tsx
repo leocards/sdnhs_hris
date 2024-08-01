9eb3a25c966d72f2a1e0fa160bfc2a4818f8aa83
leocards <leomascardenio10@gmail.com>
2024-08-01 11:09:56 +0800

    Update Index.tsx

--- resources/js/Components/page-loading/Index.tsx
+++ resources/js/Components/page-loading/Index.tsx
@@ -4,9 +4,28 @@ import { useEffect, useState } from "react";
 export default function isPageLoading(): boolean {
     const [isLoading, setIsLoading] = useState<boolean>(false)
 
+    const pages = [
+        '/dashboard', '/staff', '/leave', '/service-records', '/reports', '/messages', '/notification', '/general-search',
+        '/profile', '/profile/settings', ''
+    ]
+
+    const subpages = [
+        '/staff/new-staff/edit', '/messages?user', '/leave/view'
+    ]
+
     useEffect(() => {
         router.on('start', event => {
-            setIsLoading(true)
+            if(pages.includes(event.detail.visit.url.pathname))
+                setIsLoading(true)
+            else if(subpages.some(subpage => event.detail.visit.url.pathname.startsWith(subpage)))
+                setIsLoading(true)
+        })
+
+        router.on('finish', event => {
+            if(pages.includes(event.detail.visit.url.pathname))
+                setIsLoading(false)
+            else if(subpages.some(subpage => event.detail.visit.url.pathname.startsWith(subpage)))
+                setIsLoading(false)
         })
     }, [])
 

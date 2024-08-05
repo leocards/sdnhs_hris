929cb3de13cfc60d59b12200feb6d3bc368cce73
leocards <leomascardenio10@gmail.com>
2024-08-05 10:20:58 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        if(!in_array(Auth::user()->role, ['HR', 'HOD', 'Principal']))
+        if(!in_array(Auth::user()->role, ['HOD', 'Principal']))
         {
             return Inertia::render("Dashboard", [
                 "leaves" => Leave::with(['user:id,first_name,last_name'])->with('medical_certificate')->where('user_id', Auth::id())->orderBy('created_at', 'desc')->paginate(20),
